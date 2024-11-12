347065123364ed00b545c06af643aec29a081455
nika kobaidze <nika.kobaidze@gmail.com>
2024-11-12 11:10:40 +0400

    [ADD] login form

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
