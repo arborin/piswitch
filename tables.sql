6c188e41d8d49ad8aca78e4279a210d7f234b656
nika kobaidze <nika.kobaidze@gmail.com>
2024-11-12 09:33:02 +0400

    ADD database

--- tables.sql
+++ tables.sql
@@ -1,5 +1,29 @@
 CREATE TABLE pins (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     gpio INTEGER NOT NULL,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(10),
+    status VARCHAR(10),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pin_number INTEGER,
+    pin_name VARCHAR(255),
+    name VARCHAR(255),
+    user_name VARCHAR(255),
+    role VARCHAR(10),
+    action VARCHAR(10),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
